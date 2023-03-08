d548770c26dd02c97453a1353d8c419cfea0179d
Syauqi-S <127220930+Syauqi-S@users.noreply.github.com>
2023-03-08 12:39:49 +0700

    Update GetInputFromKeyboard.java

--- src/syauqi08032023/GetInputFromKeyboard.java
+++ src/syauqi08032023/GetInputFromKeyboard.java
@@ -15,10 +15,10 @@ public class GetInputFromKeyboard {
    public static void main( String[] args ){
         BufferedReader dataIn = new BufferedReader(new
             InputStreamReader( System.in) );
-            String name = "";
-            System.out.print("Please Enter Your Name:");
+        String name = "";
+        System.out.print("Please Enter Your Name:");
         try{
-        name = dataIn.readLine();
+            name = dataIn.readLine();
         }catch( IOException e ){
             System.out.println("Error!");
         }
