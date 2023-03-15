b6c605d0f04d89a8070a8878c975a59371bf32bb
Syauqi-S <syauqisyafiq21@gmail.com>
2023-03-15 23:00:20 +0700

    Update ContohIncrement.java

--- src/syauqi07032023/ContohIncrement.java
+++ src/syauqi07032023/ContohIncrement.java
@@ -16,30 +16,30 @@ public class ContohIncrement {
     int k = 0;
 	int inc1, inc2;
 	
-    inc1 = Increment1(i,j,k);
+    inc1 = Increment1(i,j,k); //memanggil method Increment1
 	
-	System.out.println("\nIncrement dengan ++j");
+    System.out.println("\nIncrement dengan ++j");
     System.out.println("k = " + k);
     System.out.println("j = " + j);
     System.out.println("i = " + i);
     System.out.println("Hasil inc = " + inc1);
  
 	
-	inc2 = Increment2(i,j,k);
+	inc2 = Increment2(i,j,k); //memanggil method Increment2
 	
-	System.out.println("\nIncrement dengan j++");
+    System.out.println("\nIncrement dengan j++");
     System.out.println("k = " + k);
     System.out.println("j = " + j);
     System.out.println("i = " + i);
-	System.out.println("Hasil inc = " + inc2);
+    System.out.println("Hasil inc = " + inc2);
 	
     }
-	public static int Increment1(int i,int j,int k){
+	public static int Increment1(int i,int j,int k){ //method utk ++j
 		k = ++j + i;
 		return k;
 	}
 	
-	public static int Increment2(int i,int j,int k){
+	public static int Increment2(int i,int j,int k){ //method utk j++
 		k = j++ + i;
 		return k;
 	}
